270f0e8ac4752206ebdbdb52293e675148a61d94
KamedaKei <kei@KamedaKei-no-MacBook.local>
2016-10-15 17:34:38 +0900

    hogehog

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :posts
+	resources :users do
+		resources :posts
+	end
   get 'test_page/home'
-
   get 'test_page/help'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
